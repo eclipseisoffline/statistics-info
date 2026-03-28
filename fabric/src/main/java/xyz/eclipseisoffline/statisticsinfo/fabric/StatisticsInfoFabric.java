ab80ce0af3e7b2587ebf3d243e74cb7fac164aa2
Eclipse <eclipse@eclipseisoffline.xyz>
2026-03-28 09:42:58 +0000

    Initial commit

---
+++ fabric/src/main/java/xyz/eclipseisoffline/statisticsinfo/fabric/StatisticsInfoFabric.java
@@ -0,0 +1,11 @@
+package xyz.eclipseisoffline.statisticsinfo.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StatisticsInfoFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("mod init");
+    }
+}
---
+++ neoforge/src/main/java/xyz/eclipseisoffline/statisticsinfo/neoforge/StatisticsInfoNeoForge.java
@@ -0,0 +1,11 @@
+package xyz.eclipseisoffline.statisticsinfo.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("statistics-info")
+public class StatisticsInfoNeoForge {
+
+    public StatisticsInfoNeoForge() {
+        System.out.println("mod init");
+    }
+}
